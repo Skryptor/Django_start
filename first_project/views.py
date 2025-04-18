5c6440de7829867a4fb740bca63e1f69ebcffad6
Skryptor <20Dimon19997@gmail.com>
2025-04-18 09:07:29 +0300

    fixed_bugs

--- first_project/views.py
+++ first_project/views.py
@@ -26,26 +26,26 @@ DATA = {
 }
 def Home_page(request):
     return HttpResponse("<pre>hellow, this webpage have url"
-                        "\n/current_time/ "
-                        "\n/workdir/ "
-                        "\n/recipe/ "
+                        #"\n/current_time/ "
+                        #"\n/workdir/ "
+                        #"\n/recipe/ "
                         "\n/create_phone/ "
                         "\n/catalog/ <pre>")
 
-def current_time(request):
-    return HttpResponse(f'{datetime.datetime.now().time()}')
-
-def workdir(request):
-    files = os.listdir('.')
-    return HttpResponse("<br>".join(files))
-
-def recipe(request, str_):
-    dish = []
-    sum_ = int(request.GET.get('sum_', 1))
-    for name, values in DATA[str_].items():
-        dishes = f" {name}: {values * sum_} "
-        dish.append(dishes)
-    return HttpResponse("<br>".join(dish))
+# def current_time(request):
+#     return HttpResponse(f'{datetime.datetime.now().time()}')
+#
+# def workdir(request):
+#     files = os.listdir('.')
+#     return HttpResponse("<br>".join(files))
+#
+# def recipe(request, str_):
+#     dish = []
+#     sum_ = int(request.GET.get('sum_', 1))
+#     for name, values in DATA[str_].items():
+#         dishes = f" {name}: {values * sum_} "
+#         dish.append(dishes)
+#     return HttpResponse("<br>".join(dish))
 
 def create_phone(request):
 
@@ -61,11 +61,28 @@ def create_phone(request):
 
     return HttpResponse('ready')
 def get_phone(request):
-    user_get_phone = request.GET.get('name',)
+    user_get_phone = request.GET.get('name')
+    sort_param = request.GET.get('sort')
+
     if user_get_phone:
         phone_object = Phone.objects.filter(name=user_get_phone)
     else:
         phone_object = Phone.objects.all()
+
+    if sort_param == 'name':
+        phone_object = phone_object.order_by('name')
+    elif sort_param == 'cheap':
+        phone_object = phone_object.order_by('price')
+    elif sort_param == 'expensive':
+        phone_object = phone_object.order_by('-price')
+
+    sort_buttons = """
+            <div style="margin-bottom: 20px;">
+                <a href="/catalog/?sort=name" style="margin-right: 10px;"> Название</a>
+                <a href="/catalog/?sort=cheap" style="margin-right: 10px;">⬇ Сначала дешевые</a>
+                <a href="/catalog/?sort=expensive" style="margin-right: 10px;">⬆ Сначала дорогие</a>
+            </div>
+        """
     phones = [
         f'''
         <div style="border: 1px purple; padding: 10px; margin=bottom: 20px;">
@@ -76,4 +93,4 @@ def get_phone(request):
             <strong> LTE:</strong>{'✅' if i.lte_exists else '❌'}
         </div>
         ''' for i in phone_object]
-    return HttpResponse("".join(phones))
+    return HttpResponse(sort_buttons + "".join(phones))
--- start_django/urls.py
+++ start_django/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from first_project.views import Home_page, current_time, workdir, recipe, create_phone, get_phone
+from first_project.views import Home_page, create_phone, get_phone#, current_time, workdir, recipe
 
 urlpatterns = [
     path('', Home_page),
-    path('current_time/', current_time),
-    path('workdir/', workdir),
-    path('recipe/<str:str_>', recipe),
+    # path('current_time/', current_time),
+    # path('workdir/', workdir),
+    # path('recipe/<str:str_>', recipe),
     path('create_phone/', create_phone),
     path('catalog/', get_phone),
     path('admin/', admin.site.urls),
